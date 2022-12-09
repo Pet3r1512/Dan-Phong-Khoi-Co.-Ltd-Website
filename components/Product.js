60085bdd3dc585f9190d29b175bcc84c0fa09e40
phongptt <pttp1512@gmail.com>
2022-12-09 15:49:32 +0700

    Remove unused

--- components/Product.js
+++ components/Product.js
@@ -1,4 +1,4 @@
-import react, { useState } from 'react'
+import { useState } from 'react'
 
 import ProductModal from "./modal/ProductModal"
 import ProductItem from "./product/ProductItem"
--- functions/index.js
+++
@@ -1,8 +0,0 @@
-const { default: ProductModal } = require("../components/modal/ProductModal")
-
-module.exports = {
-    useModal: (product, showModal, setShowModal) => {
-        setShowModal(true)
-        return <ProductModal showModal={showModal} setShowModal={setShowModal} product={product} />
-    }
-}
