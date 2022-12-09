e2bd49aebcb40809941970488e64529a008f9573
phongptt <pttp1512@gmail.com>
2022-12-09 15:18:41 +0700

    add Image's size

--- components/product/ProductItem.js
+++ components/product/ProductItem.js
@@ -6,7 +6,7 @@ import { formattedPrice } from "../../functions/handlers"
 export default function ProductItem({product, setShowModal, setClickedProduct}) {
     return (
         <div className="mx-auto">
-            <Image src={product.img} alt="" className="w-[300px] h-[300px] object-cover rounded-2xl" />
+            <Image src={product.img} width={300} height={300} alt="" className="w-[300px] h-[300px] object-cover rounded-2xl" />
             <h3 className="font-medium text-lg my-2">{product.name}</h3>
             <h2 className="text-lg font-medium mb-4">
                 Price: <span className="text-xl font-semibold text-red-500">{formattedPrice(product.price)} VND</span>
